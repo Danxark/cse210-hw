f7fa9b934054ce92aa55f7e7284eb0603ba2057d
danxark <dancaralonzo@gmail.com>
2023-09-25 10:24:09 -0400

    ...

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -13,11 +13,41 @@ class Program
         anEntry.Display();
         Console.WriteLine("Welcome to the Journal Program!"); 
         Console.WriteLine("Please select one of the following choices:");
-        Console.WriteLine("1. Write ");
+        Console.WriteLine("1. Write");
         Console.WriteLine("2. Display ");
         Console.WriteLine("3. Load ");
         Console.WriteLine("4. Save ");
         Console.WriteLine("5. Quit ");
+        string answer = Console.ReadLine();
+        int useroption = int.Parse(answer);
+        if (useroption == 1)
+        {
+
+        }
+        else if (useroption == 2)
+        {
+
+        }
+        else if (useroption == 3)
+        {
+
+        }
+        else if(useroption == 4)
+        {
+
+        }
+        else if (useroption == 5)
+        {
+
+        }
+        else 
+        {
+            Console.WriteLine("Please select another option");
+        }
+
+
+
+
         
 
     }
