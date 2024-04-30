96422c182a3a1504241e09d663d2febb609ff361
Danxark <dancaralonzo@gmail.com>
2024-04-30 13:42:45 -0400

    done

---
+++ prepare/Learning02/Journal.cs
@@ -0,0 +1,24 @@
+public class Journal
+(
+    public List<Entry> _entry;
+
+    public void AddEntry(Entry newEntry)
+
+    {
+
+    }
+
+    public void DisplayAll()
+    {
+
+    }
+
+    public void SaveToFile(string file)
+    {
+
+    }
+    public void LoadFromFile(string file)
+    {
+        
+    }
+)
