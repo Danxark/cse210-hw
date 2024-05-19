16e25b2ae579407a62b5bc490153da2e71cf6513
Danxark <144806033+Danxark@users.noreply.github.com>
2024-05-18 19:40:08 -0500

    final done

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -8,19 +8,19 @@ class Program
     {
         Console.Clear();
         Console.Write("\n -Welcome to the Mindfulness Program-\n");
-        //Call Choices
+        
         Choices choice = new Choices();
         int seconds;
 
         int action = 0;
         while (action != 4)
         {
-            // Ask for user input (1-4)
+            
             action = choice.UserChoice();
             switch (action)
             {
                 case 1:
-                    // Start Breathing Activity
+                    
                     Console.Clear();
                     BreathingActivity breathing = new BreathingActivity("Breathing", 0);
                     breathing.GetActivityName();
@@ -31,7 +31,7 @@ class Program
                     breathing.GetDone();
                     break;
                 case 2:
-                    //Start Reflecting Activity
+                    
                     Console.Clear();
                     ReflectingActivity reflecting = new ReflectingActivity("Reflecting", 0);
                     reflecting.GetActivityName();
@@ -42,7 +42,7 @@ class Program
                     reflecting.GetDone();
                     break;
                 case 3:
-                    //Start Listing Activity
+                   
                     Console.Clear();
                     ListingActivity listing = new ListingActivity("Listing", 0);
                     listing.GetActivityName();
@@ -53,7 +53,7 @@ class Program
                     listing.GetDone();
                     break;
                 case 4:
-                    // Quite
+                 ¿
                     Console.WriteLine("\nThank you for using the Mindfulness Program!\n");
                     break;
                 default:
